aa0e5e43c9a60ad791c7c999da5a77477694a010
Mykhailo Vasylkov <vasilkoff.mihail@gmail.com>
2025-01-16 16:58:04 +0000

    Fix identation error in apps.py

--- about/apps.py
+++ about/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class AboutConfig(AppConfig):
-     """
+    """
     Configures the 'about' application.
 
     Attributes:
--- blog/apps.py
+++ blog/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
 
+
 class BlogConfig(AppConfig):
     """
-    Configures the 'blog' application.
-
-    Attributes:
-        default_auto_field (str): Default primary key type ('BigAutoField').
-        name (str): Application name ('blog').
+    Provides primary key type for blog app
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
